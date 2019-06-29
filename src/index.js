b56f346ceba9dcdf2c6f98affc253189c1dc9421
Siddharth <singhsid478@gmail.com>
2019-06-29 14:17:05 +0530

    added express

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+
 const app = express();
 
 
 app.listen(3000, () => {
-    console.log('Server started at port 3000...')
-})
+  console.log('Server started at port 3000...');
+});
+
